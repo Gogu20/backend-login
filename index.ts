7f728ff073343abf1f47b2c3eedec8dadf8848af
Mierlut Timotei <mierluttimotei@Mierluts-MacBook-Pro.local>
2023-11-29 12:57:16 +0200

    register done

---
+++ index.ts
@@ -0,0 +1,32 @@
+import { Express, Request, Response } from 'express';
+const express = require('express');
+const app: Express = express();
+const bcrypt = require('bcrypt');
+require('dotenv').config();
+
+app.use(express.json());
+
+const users = [];
+
+app.get('/users', (req: Request, res: Response) => {
+    res.json(users);
+}) //request users data for testing
+
+app.post('/users', async (req: Request, res: Response) => {
+    try {
+        const dupeEmail = users.find(dupeEmail => dupeEmail.email == req.body.email);
+        if(dupeEmail != null) {
+            return res.status(400).send("Email already in use.")
+        }
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = {email: req.body.email, password: hashedPassword};
+        users.push(user);
+        res.status(201).send("User created successfully.");
+    } catch {
+        res.status(500).send()
+    }
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running at ${process.env.HOST}: ${process.env.PORT}`);
+})
