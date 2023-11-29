7550292d58596b407091f1540dc663d5c90737ac
Mierlut Timotei <mierluttimotei@Mierluts-MacBook-Pro.local>
2023-11-29 15:04:01 +0200

    login is also done now

--- index.ts
+++ index.ts
@@ -13,19 +13,36 @@ app.get('/users', (req: Request, res: Response) => {
 }) //request users data for testing
 
 app.post('/users', async (req: Request, res: Response) => {
+    const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$/i; 
+    if (!expression.test(req.body.email)) {
+        return res.status(400).send("Invalid email.");
+    }
+    const dupeEmail = users.find(dupeEmail => dupeEmail.email == req.body.email);
+    if(dupeEmail != null) {
+        return res.status(400).send("Email already in use.")
+    }
     try {
-        const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$/i; 
-        if (!expression.test(req.body.email)) {
-            return res.status(400).send("Invalid email.");
-        }
-        const dupeEmail = users.find(dupeEmail => dupeEmail.email == req.body.email);
-        if(dupeEmail != null) {
-            return res.status(400).send("Email already in use.")
-        }
+        
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
         const user = {email: req.body.email, password: hashedPassword};
         users.push(user);
         res.status(201).send("User created successfully.");
+    } catch {
+        res.status(500).send()
+    }
+})
+
+app.post('/users/login', async (req: Request, res: Response) => {
+    const user = users.find(user => user.email == req.body.email)
+    if (user == null) {
+        return res.status(404).send("User does not exist.");
+    }
+    try {
+        if (bcrypt.compare(req.body.password, user.password)) {
+            res.send("Logged in successfully.");
+        } else {
+            res.status(401).send("Incorrect password.");
+        }
     } catch {
         res.status(500).send()
     }
