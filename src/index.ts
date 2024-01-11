011aab2dc8e272ebae7a47b499774ff71a74cf09
Mierlut Timotei <mierluttimotei@Mierluts-MacBook-Pro.local>
2024-01-11 10:57:43 +0200

    big fix: bundleErrorsFromArray() now has a check to see if the passed parameter is not undefined, hopefully it's going to stop breaking after every commit now. The directory has been restructured. addUser() is now async

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { Express, Request, Response } from 'express';
-import { User, UserInput, ValidationResult } from '../resources/sharedTypes'
-import { isUser } from '../resources/typeUtils';
-import { UserValidation } from '../resources/UserValidation';
-import { UserActions } from '../resources/UserActions';
-import { UserData } from '../resources/UserData';
-import { bundleErrorsFromArray } from '../resources/generalUtils';
+import { User, UserInput, ValidationResult } from './sharedTypes'
+import { TypeUtils } from './utils/TypeUtils';
+import { UserValidation } from './user/UserValidation';
+import { UserActions } from './user/UserActions';
+import { UserData } from './user/UserData';
+import { GeneralUtils } from './utils/GeneralUtils';
 
 
 const express = require('express');
@@ -15,7 +15,9 @@ app.use(express.json());
 
 const userData = new UserData;
 const userActions = new UserActions(userData);
-const userValidation = new UserValidation();
+const userValidation = new UserValidation;
+const typeUtils = new TypeUtils;
+const utils = new GeneralUtils;
 
 //request users data for testing
 app.get('/users', (req: Request, res: Response) => {
@@ -28,14 +30,14 @@ app.post('/users/register', async (req: Request, res: Response) => {
         password: req.body.password
     }
     const currentUser: User | undefined = userData.getUserByEmail(userInputData.email);
-    const userAlreadyExists = isUser(currentUser);
+    const userAlreadyExists = typeUtils.isUser(currentUser);
     if (userAlreadyExists) {
         return res.status(409).send("Email already in use.");
     }
 
     const validationResults: ValidationResult = userValidation.registerValidation(userInputData);
     const thereIsError: boolean = !validationResults.success;
-    const validationErrors = bundleErrorsFromArray(validationResults.errors as string[])
+    const validationErrors: string | undefined = utils.bundleErrorsFromArray(validationResults.errors)
     if (thereIsError) {
         return res.status(400).send(validationErrors);
     }
@@ -63,7 +65,7 @@ app.post('/users/login', async (req: Request, res: Response) => {
         return res.status(400).send(validationResults.errors);
     }
     const currentUser: User | undefined = userData.getUserByEmail(userInputData.email);
-    const userDoesNotExists = !isUser(currentUser);
+    const userDoesNotExists = !typeUtils.isUser(currentUser);
     if (userDoesNotExists) {
         return res.status(404).send("User does not exist.");
     }
